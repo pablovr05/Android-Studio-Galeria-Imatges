8d359c09c27f89795b68bed9716e0a07e367a82c
pablo <pablovicenteroura2005@gmail.com>
2024-12-02 16:35:51 +0100

    botó obrir cámera

--- app/src/main/java/com/pablovicente/myapplication/MainActivity.java
+++ app/src/main/java/com/pablovicente/myapplication/MainActivity.java
@@ -1,12 +1,17 @@
 package com.pablovicente.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,38 +19,73 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityResultLauncher<Intent> someActivityResultLauncher;
+    private ActivityResultLauncher<Intent> cameraResultLauncher;
     private ImageView imageView;
+    private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializa el ImageView
         imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
 
-        // Registra el lanzador para manejar el resultado
         someActivityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        // Obtén la URI de la imagen seleccionada
-                        Uri uri = result.getData().getData();
-                        // Establece la imagen seleccionada en el ImageView
-                        imageView.setImageURI(uri);
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Uri uri = data.getData();
+                            imageView.setImageURI(uri);
+                        }
                     }
                 }
+            }
         );
+
+        cameraResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+
+                        Bitmap image = (Bitmap) data.getExtras().get("data");
+
+                        imageView.setImageBitmap(image);
+                    }
+                }
+            }
+        );
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSomeActivityForResult();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraResultLauncher.launch(intent);
+            }
+        });
     }
 
-    // Método para abrir la galería
-    public void openSomeActivityForResult(View view) {
-        // Crea un Intent para seleccionar contenido
+    private void openSomeActivityForResult() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*"); // Permitir todos los formatos de imagen
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true); // Solo archivos locales
-        // Lanza la actividad para obtener el resultado
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
         someActivityResultLauncher.launch(intent);
     }
 }
-
