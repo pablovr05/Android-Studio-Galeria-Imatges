f33da8338b0aee146ec6f2415c46b17164ce8abf
pablo <pablovicenteroura2005@gmail.com>
2024-12-11 20:16:28 +0100

    final commit

--- app/src/main/java/com/pablovicente/myapplication/MainActivity.java
+++ app/src/main/java/com/pablovicente/myapplication/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,16 +17,20 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityResultLauncher<Intent> someActivityResultLauncher;
     private ActivityResultLauncher<Intent> cameraResultLauncher;
+    private ActivityResultLauncher<Intent> cameraResultLauncherFullSize;
     private ImageView imageView;
     private Button button;
     private Button button2;
     private Button button3;
-
+    private Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +74,31 @@ public class MainActivity extends AppCompatActivity {
             }
         );
 
+        cameraResultLauncherFullSize = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                        ImageView imageView = findViewById(R.id.imageView);
+                        Intent data = result.getData();
+                        Bundle extras = data.getExtras();
+                        if( extras!=null ) {
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            imageView.setImageBitmap(imageBitmap);
+                            return;
+                        } else {
+                            if( photoURI!=null ) {
+                                imageView.setImageURI(photoURI);
+                                return;
+                            }
+                            Log.e("ERROR","No hi ha cap photoURI");
+                        }
+                    }
+                }
+            }
+        );
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -86,7 +117,15 @@ public class MainActivity extends AppCompatActivity {
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String filePath = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString() + "/tmpImg.jpg";
+                File photoFile = new File( filePath );
+                photoURI = FileProvider.getUriForFile(MainActivity.this,
+                        "com.pablovicente.myapplication.fileprovider",
+                        photoFile);
 
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                cameraResultLauncherFullSize.launch(intent);
             }
         });
     }
---
+++ app/src/main/java/com/pablovicente/myapplication/MyFileProvider.java
@@ -0,0 +1,7 @@
+package com.pablovicente.myapplication;
+
+import androidx.core.content.FileProvider;
+
+public class MyFileProvider extends FileProvider {
+
+}
